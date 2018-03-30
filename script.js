82165cf8e8d36e5a9c436d07569d48aa7ddb8174
VictoriaDoroshko <36131809+VictoriaDoroshko@users.noreply.github.com>
2018-03-30 11:41:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function quickSort(arr, low, high) {
+        var i = low;                
+        var j = high;
+        var middle = arr[ Math.round(( low + high ) / 2) ];  
+        do {
+            while(arr[i] < middle)
+              {
+                ++i;  
+              } 
+            while(arr[j] > middle)
+              {
+                --j; 
+              }
+            if(i <= j){           
+                var temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++; j--;
+            }
+        } 
+        while(i < j);
+        
+        if(low < j){
+          quickSort(arr, low, j);
+        } 
+        if(i < high){
+          quickSort(arr, i, high);
+        } 
+    }
